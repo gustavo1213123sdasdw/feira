682044f3a470c53667a66e6e638db81896d2dc9b
gustavo1213123sdasdw <142310535+gustavo1213123sdasdw@users.noreply.github.com>
2024-08-01 08:03:25 -0300

    Add files via upload

---
+++ feira/script.js
@@ -0,0 +1,5 @@
+// Atualmente não há JavaScript interativo incluído, mas você pode adicionar funcionalidades aqui conforme necessário
+document.addEventListener('DOMContentLoaded', () => {
+    // Exemplo de código para exibir um alerta quando a página é carregada
+    console.log("Página carregada!");
+});
